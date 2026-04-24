30ed35d593e26769ef5dcf55e263a56c96494ea0
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-24 22:29:53 +0600

    root added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Layouts/Root';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </StrictMode>,
 )
