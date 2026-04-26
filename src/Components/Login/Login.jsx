d71ffbad4880a899b530109838dbc0fea953fb0a
impressivecoder1 <rohanrahman251@gmail.com>
2026-04-26 22:37:26 +0600

    implement google singin mathod

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,16 @@
 // import React from 'react';
+import { GoogleAuthProvider } from "firebase/auth";
 
+const provider = new GoogleAuthProvider();
 const Login = () => {
+    const handleGoogleSingIn = () => {
+        console.log('gooogle ')
+    }
     return (
         <div className="w-3xl mx-auto bg-gray-500">
             <h1 className="text-center text-2xl bg-gray-300 py-4 rounded-xl">This is login page</h1>
+            <div></div>
+            <button onClick={() => handleGoogleSingIn()} className=" text-center">Sign in with google</button>
         </div>
     );
 };
